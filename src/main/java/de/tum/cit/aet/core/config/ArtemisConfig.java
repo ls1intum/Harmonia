8f19afb16ce38d5e09b3fbe5514b6f2ff83a3470
Cathy <catherine.kalra@tum.de>
2025-11-17 01:29:39 +0100

    replace Value annotations with ConfigurationProperties

--- src/main/java/de/tum/cit/aet/core/config/ArtemisConfig.java
+++ src/main/java/de/tum/cit/aet/core/config/ArtemisConfig.java
@@ -2,32 +2,20 @@ package de.tum.cit.aet.core.config;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @Configuration
+@ConfigurationProperties(prefix = "artemis")
 @Getter
 @Setter
 public class ArtemisConfig {
 
-    @Value("${artemis.baseUrl}")
     private String baseUrl;
-
-    @Value("${artemis.username}")
     private String username;
-
-    @Value("${artemis.password}")
     private String password;
-
-    @Value("${artemis.jwtToken}")
     private String jwtToken;
-
-    @Value("${artemis.exerciseId}")
     private Long exerciseId;
-
-    @Value("${artemis.gitRepoPath}")
     private String gitRepoPath;
-
-    @Value("${artemis.numThreads}")
     private Integer numThreads;
 }
