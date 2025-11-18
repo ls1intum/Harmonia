d91c175a731507b3119f9138f6db2388262dd234
Cathy <catherine.kalra@tum.de>
2025-11-18 23:31:31 +0100

    `General`: Add repository fetching (#8)
    * add repository fetching
    
    * split DTOs into separate files
    
    * replace Value annotations with ConfigurationProperties
    
    * fix spotless violations & move methods
    
    * fix spotless violations
    
    * fix openapi workflow
    
    * chore: update OpenAPI spec and generated client
    
    * update TeamRepositoryDTO
    
    * implement changes requested
    
    * update spotless suggestions
    
    ---------
    
    Co-authored-by: Aniruddh Zaveri <92953467+az108@users.noreply.github.com>
    Co-authored-by: aniruddhzaveri <aniruddh.zaveri@tum.de>
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- src/test/java/de/tum/cit/aet/HarmoniaApplicationTests.java
+++ src/test/java/de/tum/cit/aet/HarmoniaApplicationTests.java
@@ -1,10 +1,37 @@
 package de.tum.cit.aet;
 
+import de.tum.cit.aet.repositoryProcessing.dto.ParticipationDTO;
+import de.tum.cit.aet.repositoryProcessing.dto.TeamRepositoryDTO;
+import de.tum.cit.aet.repositoryProcessing.service.ArtemisClientService;
+import de.tum.cit.aet.repositoryProcessing.service.RepositoryFetchingService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class HarmoniaApplicationTests {
+
+    @Autowired
+    private ArtemisClientService artemisClientService;
+
+    @Autowired
+    private RepositoryFetchingService repositoryFetchingService;
+
+    @Test
+    void testRepositoryCloningAndPulling() {
+        List<TeamRepositoryDTO> teamRepositories = repositoryFetchingService.fetchAndCloneRepositories();
+        System.out.println(teamRepositories);
+    }
+
+    @Test
+    void testRepositoryFetching() {
+        List<ParticipationDTO> teamRepositories = artemisClientService.fetchParticipations();
+        for (ParticipationDTO participationDTO : teamRepositories) {
+            System.out.println(participationDTO);
+        }
+    }
 
     @Test
     void contextLoads() {
