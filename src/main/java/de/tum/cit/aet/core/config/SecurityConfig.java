12fea4aee97da57a36030a7dad7b84d41d7d14fe
aniruddhzaveri <aniruddh.zaveri@tum.de>
2025-12-25 19:50:31 +0100

    adjust for security config cors

--- src/main/java/de/tum/cit/aet/core/config/SecurityConfig.java
+++ src/main/java/de/tum/cit/aet/core/config/SecurityConfig.java
@@ -15,11 +15,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import java.util.List;
 
 @Configuration
 @EnableWebSecurity
